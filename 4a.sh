eed4d267177e4b9726d46d0792d077e63e9c0bfb
Andrey Ivanov <ans.ivanov@gmail.com>
2014-11-23 05:15:55 +0300

    4b done

--- 4a.sh
+++ 4a.sh
@@ -2,17 +2,20 @@
 LOGFILE="log.txt"
 TMPFILE=`mktemp /tmp/hhLogs.XXXXXX` || exit 1
 
-grep " 12:.* /resume[?/ ].* 200 " $LOGFILE | sed -E 's/^.* (.*)ms/\1/' | sort >> $TMPFILE
-
-totalTime="$(cat $TMPFILE | paste -sd+ - | bc -l)"
-echo "общее время успешных обращений: ${totalTime}ms"
-
+grep " 12:.* /resume\?.* 200 " $LOGFILE | sed -E 's/^.* (.*)ms/\1/' | sort > $TMPFILE
 count="$(cat $TMPFILE | wc -l)"
-echo "среднее значение: "$(echo "${totalTime} / ${count}" | bc -l)"ms"
 
-q95Index=$(echo "${count} - 5 * ${count} / 100" | bc) #не "*0.05" чтобы не разбираться с округлением
-q99Index=$(echo "${count} - ${count} / 100"| bc)
-echo "95% квантиль: "$(sed "${q95Index}q;d" $TMPFILE)"ms"
-echo "99% квантиль: "$(sed "${q99Index}q;d" $TMPFILE)"ms"
+if [ $count -gt 0 ]
+then
+    totalTime="$(cat $TMPFILE | paste -sd+ - | bc -l)"
+    echo "общее время успешных обращений: ${totalTime}ms"
+    echo "среднее значение: "$(echo "${totalTime} / ${count}" | bc -l)"ms"
 
+    q95Index=$(echo "${count} - ${count} / 20" | bc) #не "*0.05" чтобы не разбираться с округлением
+    q99Index=$(echo "${count} - ${count} / 100" | bc)
+    echo "95% квантиль: "$(sed "${q95Index}q;d" $TMPFILE)"ms"
+    echo "99% квантиль: "$(sed "${q99Index}q;d" $TMPFILE)"ms"
+else
+    echo "данных нет" 
+fi
 rm -f $TMPFILE
---
+++ 4b.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOGFILE="log.txt"
+DAY="2013-01-18"
+TMPFILE=`mktemp /tmp/hhLogs.XXXXXX` || exit 1
+
+grep "${DAY} .* /resume\?.*id=43" $LOGFILE | sed -E 's/^.* (.*)ms/\1/' | sort > $TMPFILE
+count="$(cat $TMPFILE | wc -l)"
+if [ $count -gt 0 ]
+then
+
+    totalTime="$(cat $TMPFILE | paste -sd+ - | bc -l)"
+    echo "среднее значение: "$(echo "${totalTime} / ${count}" | bc -l)"ms"
+
+    medianaIndex=$(echo "${count} / 2 + 1" | bc)
+    echo "медиана: "$(sed "${medianaIndex}q;d" $TMPFILE)"ms"
+else
+    echo "данных нет" 
+fi
+rm -f $TMPFILE
